18623103c341bd64b8df977198a36a1997435556
szymslow12 <szym.slow12@gmail.com>
2018-09-11 21:22:12 +0200

    add DoubleLinkedNode with data,nextNode,previousNode,index,setters/getters

---
+++ src/main/java/com/codecool/linkedList/DoubleLinkedNode.java
@@ -0,0 +1,46 @@
+package com.codecool.linkedList;
+
+public class DoubleLinkedNode<T> {
+
+    private T data;
+    private DoubleLinkedNode<T> nextNode;
+    private DoubleLinkedNode<T> previousNode;
+    private int index;
+
+    public DoubleLinkedNode(T data, int index) {
+        this.data = data;
+        this.index = index;
+    }
+
+    public DoubleLinkedNode<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(DoubleLinkedNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public DoubleLinkedNode<T> getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(DoubleLinkedNode<T> previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
