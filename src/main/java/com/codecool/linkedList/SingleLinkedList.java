183805588a8c50cdbb6392c294135b259db9699f
szymslow12 <szym.slow12@gmail.com>
2018-09-11 12:25:35 +0200

    fix type, implement add(T data)

--- src/main/java/com/codecool/linkedList/SingleLinkedList.java
+++ src/main/java/com/codecool/linkedList/SingleLinkedList.java
@@ -3,12 +3,32 @@ package com.codecool.linkedList;
 public class SingleLinkedList<T> {
 
     private SingleLinkedNode<T> head;
-    private SingleLinkedList<T> last;
+    private SingleLinkedNode<T> last;
     private int length;
 
     public SingleLinkedList() {
         head = null;
         last = null;
         length = 0;
+    }
+
+    public void add(T data) {
+        SingleLinkedNode<T> node = new SingleLinkedNode<>(data);
+
+        if (head == null) {
+            head = node;
+            last = node;
+            length++;
+            return;
+        }
+
+        /*SingleLinkedNode<T> currentNode = head;
+
+        while (currentNode.getNextNode() != null) {
+            currentNode = currentNode.getNextNode();
+        }*/
+
+        last.setNextNode(node);
+        last = node;
     }
 }
