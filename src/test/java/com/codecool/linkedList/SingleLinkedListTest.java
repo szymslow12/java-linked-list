be309b91b38dad4e1c1f8429474b2253edfe22b1
szymslow12 <szym.slow12@gmail.com>
2018-09-11 13:46:53 +0200

    change name testRemove -> testRemoveFirstItem, add testRemoveLastItem

--- src/test/java/com/codecool/linkedList/SingleLinkedListTest.java
+++ src/test/java/com/codecool/linkedList/SingleLinkedListTest.java
@@ -26,16 +26,33 @@ class SingleLinkedListTest {
 
 
     @Test
-    void testRemove() {
+    void testRemoveFirstItem() {
         SingleLinkedList<Integer> integerList = getIntegerList();
 
-        String expected = "1 2 3 4 5 6 8 9";
+        String expected = "1 2 3 4 5 6 7 8 9";
 
         integerList.remove(0);
-        integerList.remove(7);
 
         String actual = integerList.toString();
 
         assertEquals(expected, actual);
     }
+
+
+    @Test
+    void testRemoveLastItem() {
+        SingleLinkedList<Integer> integerList = getIntegerList();
+
+        String expected = "0 1 2 3 4 5 6 7 8";
+
+        integerList.remove(9);
+
+        String actual = integerList.toString();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+
 }
