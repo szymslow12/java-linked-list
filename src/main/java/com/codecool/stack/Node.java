f567240ef98d75c4c90ff12e693bb5a8de90b3d6
szymslow12 <szym.slow12@gmail.com>
2018-09-12 14:50:51 +0200

    add Node<T> with T data, Node<T> nextNode, setters/getters

---
+++ src/main/java/com/codecool/stack/Node.java
@@ -0,0 +1,28 @@
+package com.codecool.stack;
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+        nextNode = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+}
