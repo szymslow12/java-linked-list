f6a7b22366116f7fb559bed83ecff0e724717416
szymslow12 <szym.slow12@gmail.com>
2018-09-12 18:43:50 +0200

    add Node head,tail,constructor,enqueue,peek,dequeue,queueSize,isEmpty

--- src/main/java/com/codecool/queue/CustomQueue.java
+++ src/main/java/com/codecool/queue/CustomQueue.java
@@ -1,4 +1,37 @@
 package com.codecool.queue;
 
 public class CustomQueue {
+
+    private Node head;
+    private Node tail;
+
+    public CustomQueue() {
+        head = null;
+        tail = null;
+    }
+
+
+    public void enqueue(String value) {
+
+    }
+
+
+    public String peek() {
+        return "";
+    }
+
+
+    public String dequeue() {
+        return "";
+    }
+
+
+    public int queueSize() {
+        return 0;
+    }
+
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
