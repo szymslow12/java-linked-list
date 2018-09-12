b76da1bc8edd24d9ae441c5d4f97fcb5d8408f48
szymslow12 <szym.slow12@gmail.com>
2018-09-12 15:28:56 +0200

    add StackUnderflow

---
+++ src/main/java/com/codecool/exceptions/StackUnderflow.java
@@ -0,0 +1,10 @@
+package com.codecool.exceptions;
+
+public class StackUnderflow extends Exception {
+
+    public StackUnderflow() {}
+
+    public StackUnderflow(String message) {
+        super(message);
+    }
+}
