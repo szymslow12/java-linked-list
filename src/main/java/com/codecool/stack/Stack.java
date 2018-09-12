8f2e1bbb5655d5524d9ee7500bbb0184a0161154
szymslow12 <szym.slow12@gmail.com>
2018-09-12 14:31:59 +0200

    add push, toString, int nextIndex

--- src/main/java/com/codecool/stack/Stack.java
+++ src/main/java/com/codecool/stack/Stack.java
@@ -6,11 +6,40 @@ public class Stack<T> {
 
     private int stackSize;
     private DoubleLinkedNode<T> topNode;
+    private int nextIndex;
 
     public Stack(int size) {
         stackSize = size;
         topNode = null;
+        nextIndex = 0;
     }
 
+    public void push(T data) {
+        if (nextIndex == stackSize) {
+            throw new StackOverflowError("Stack is full, can't add item!");
+        } else {
+            DoubleLinkedNode<T> newNode = new DoubleLinkedNode<>(data, nextIndex++);
+            if (topNode == null) {
+                topNode = newNode;
+                topNode.setPreviousNode(null);
+                return;
+            }
 
+            topNode.setNextNode(newNode);
+            newNode.setPreviousNode(topNode);
+            topNode = newNode;
+        }
+    }
+
+    public String toString() {
+        StringBuilder sB = new StringBuilder(topNode != null ? topNode.toString(): "");
+        DoubleLinkedNode<T> currentNode = topNode;
+
+        while (currentNode.getPreviousNode() != null) {
+            DoubleLinkedNode<T> previousNode = currentNode.getPreviousNode();
+            sB.append(previousNode.toString());
+            currentNode = previousNode;
+        }
+        return sB.toString();
+    }
 }
