bf114d4e9f5cf3ce5409a97f2e4ffa8932d228d2
szymslow12 <szym.slow12@gmail.com>
2018-09-13 17:20:09 +0200

    change CustomQueue is now Priority Queue -> return items by priority

--- src/main/java/com/codecool/queue/CustomQueue.java
+++ src/main/java/com/codecool/queue/CustomQueue.java
@@ -13,11 +13,13 @@ public class CustomQueue {
         head = null;
         tail = null;
         size = 0;
+        highestPriority = 0;
     }
 
 
     public void enqueue(String value, Integer priority) {
         Node newNode = new Node(value, priority);
+        highestPriority = priority > highestPriority ? priority: highestPriority;
         size++;
         if (head == null) {
             head = newNode;
@@ -34,7 +36,7 @@ public class CustomQueue {
         if (head == null) {
             throw new QueueIsEmpty("Can't peek! Queue is empty!");
         }
-        return head.getValue();
+        return getNodeWithHighestPriority().getValue();
     }
 
 
@@ -42,10 +44,54 @@ public class CustomQueue {
         if (head == null) {
             throw new QueueIsEmpty("Can't dequeue! Queue is empty!");
         }
-        String value = head.getValue();
-        head = head.getNextNode();
+        Node highestPriorityNode = getNodeWithHighestPriority();
+        String value = highestPriorityNode.getValue();
+        changeNodePointerBeforeHighestPriorityNode(highestPriorityNode);
+        highestPriority = findHighestPriority();
         size--;
         return value;
+    }
+
+
+    private Node getNodeWithHighestPriority() {
+        Node currentNode = head;
+
+        while (currentNode != null) {
+            Node nextNode = currentNode.getNextNode();
+            if (currentNode.getPriority() == highestPriority) {
+                return currentNode;
+            }
+            currentNode = nextNode;
+        }
+        return null;
+    }
+
+
+    private void changeNodePointerBeforeHighestPriorityNode(Node highestPriorityNode) {
+        Node currentNode = head;
+
+        while (currentNode.getNextNode() != null) {
+            Node nextNode = currentNode.getNextNode();
+            if (nextNode == highestPriorityNode) {
+                currentNode.setNextNode(highestPriorityNode.getNextNode());
+                highestPriorityNode.setNextNode(null);
+            }
+            currentNode = nextNode;
+        }
+    }
+
+
+    private int findHighestPriority() {
+        Node currentNode = head;
+        int highest = currentNode.getPriority();
+        while (currentNode.getNextNode() != null) {
+            Node nextNode = currentNode.getNextNode();
+            if (nextNode.getPriority() > highest) {
+                highest = nextNode.getPriority();
+            }
+            currentNode = nextNode;
+        }
+        return highest;
     }
 
 
