2134019c65a942068d9a14f54dbd3bc18319c8e6
szymslow12 <szym.slow12@gmail.com>
2018-09-14 18:57:23 +0200

    add getHeap

--- src/test/java/com/codecool/heap/HeapTest.java
+++ src/test/java/com/codecool/heap/HeapTest.java
@@ -4,4 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeapTest {
 
+    private Heap getHeap(int size) {
+        Heap heap = new Heap(size);
+        for (int i = 0; i < size; i++) {
+            heap.add(i);
+        }
+        return heap;
+    }
+
 }
