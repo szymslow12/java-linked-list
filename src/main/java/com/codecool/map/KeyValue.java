cf69805d4d425ebffed76e3ea6748220828fac37
szymslow12 <szym.slow12@gmail.com>
2018-09-15 13:18:41 +0200

    add constructor

--- src/main/java/com/codecool/map/KeyValue.java
+++ src/main/java/com/codecool/map/KeyValue.java
@@ -5,6 +5,12 @@ public class KeyValue {
     public String key;
     public Integer value;
 
+    public KeyValue(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+
     public String toString() {
         return key + ": " + value;
     }
