a8c6b263c2755abdbe27a5d793d540afa4a5c71c
szymslow12 <szym.slow12@gmail.com>
2018-09-15 12:12:47 +0200

    add KeyValue with String key, Integer value and toString()

---
+++ src/main/java/com/codecool/map/KeyValue.java
@@ -0,0 +1,11 @@
+package com.codecool.map;
+
+public class KeyValue {
+
+    public String key;
+    public Integer value;
+
+    public String toString() {
+        return key + ": " + value;
+    }
+}
